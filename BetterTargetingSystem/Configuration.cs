863d3aa74307a438fc30bc761dc4b589de87e238
Lynesth <lyn@nautile.nc>
2023-02-13 17:49:40 +1100

    Formatting

--- BetterTargetingSystem/Configuration.cs
+++ BetterTargetingSystem/Configuration.cs
@@ -4,30 +4,22 @@ using Dalamud.Plugin;
 using BetterTargetingSystem.Keybinds;
 using System;
 
-namespace BetterTargetingSystem
+namespace BetterTargetingSystem;
+
+[Serializable]
+public class Configuration : IPluginConfiguration
 {
-    [Serializable]
-    public class Configuration : IPluginConfiguration
-    {
-        public int Version { get; set; } = 0;
+    public int Version { get; set; } = 0;
+    public Keybind TabTargetKeybind { get; set; } = new Keybind(VirtualKey.TAB, false, false, false);
+    public Keybind ClosestTargetKeybind { get; set; } = new Keybind(VirtualKey.TAB, false, true, false);
+    public Keybind LowestHealthTargetKeybind { get; set; } = new Keybind(VirtualKey.TAB, true, false, false);
+    public Keybind BestAOETargetKeybind { get; set; } = new Keybind(null, false, false, false);
 
-        public Keybind TabTargetKeybind { get; set; } = new Keybind(VirtualKey.TAB, false, false, false);
-        public Keybind ClosestTargetKeybind { get; set; } = new Keybind(VirtualKey.TAB, false, true, false);
-        public Keybind LowestHealthTargetKeybind { get; set; } = new Keybind(VirtualKey.TAB, true, false, false);
-        public Keybind BestAOETargetKeybind { get; set; } = new Keybind(null, false, false, false);
+    // the below exist just to make saving less cumbersome
+    [NonSerialized]
+    private DalamudPluginInterface? PluginInterface;
 
-        // the below exist just to make saving less cumbersome
-        [NonSerialized]
-        private DalamudPluginInterface? PluginInterface;
+    public void Initialize(DalamudPluginInterface pluginInterface) => this.PluginInterface = pluginInterface;
 
-        public void Initialize(DalamudPluginInterface pluginInterface)
-        {
-            this.PluginInterface = pluginInterface;
-        }
-
-        public void Save()
-        {
-            this.PluginInterface!.SavePluginConfig(this);
-        }
-    }
+    public void Save() => this.PluginInterface!.SavePluginConfig(this);
 }
